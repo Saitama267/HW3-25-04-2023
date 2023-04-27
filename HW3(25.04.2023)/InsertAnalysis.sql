8e501ce8ac186e496fa01ed5c9c21ac413e90b90
Saitama267 <85219557+Saitama267@users.noreply.github.com>
2023-04-27 17:26:18 +0200

    Done
    Done all tasks

---
+++ HW3(25.04.2023)/InsertAnalysis.sql
@@ -0,0 +1,22 @@
+INSERT INTO Analysis (an_name, an_cost, an_price, an_group)
+VALUES
+('Analysis 1', 10.00, 20.00, 1),
+('Analysis 2', 15.00, 25.00, 1),
+('Analysis 3', 20.00, 30.00, 2),
+('Analysis 4', 25.00, 35.00, 2),
+('Analysis 5', 30.00, 40.00, 3),
+('Analysis 6', 35.00, 45.00, 3),
+('Analysis 7', 40.00, 50.00, 4),
+('Analysis 8', 45.00, 55.00, 4),
+('Analysis 9', 50.00, 60.00, 5),
+('Analysis 10', 55.00, 65.00, 5),
+('Analysis 11', 60.00, 70.00, 6),
+('Analysis 12', 65.00, 75.00, 6),
+('Analysis 13', 70.00, 80.00, 7),
+('Analysis 14', 75.00, 85.00, 7),
+('Analysis 15', 80.00, 90.00, 8),
+('Analysis 16', 85.00, 95.00, 8),
+('Analysis 17', 90.00, 100.00, 9),
+('Analysis 18', 95.00, 105.00, 9),
+('Analysis 19', 100.00, 110.00, 10),
+('Analysis 20', 105.00, 115.00, 10);
